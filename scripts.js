7914762a8a44eb756b68f2a97e007b8845c8f33c
Matt Shoaf <mtshoaf@gmail.com>
2025-10-12 17:40:06 -0500

    checkboxes

--- scripts.js
+++ scripts.js
@@ -34,6 +34,14 @@ document.addEventListener('DOMContentLoaded', function() {
   function updateVisuals() {
     checkboxes.forEach(cb => {
       cb.checked = checkedState[cb.id] || false;
+      const wrapper = cb.previousElementSibling;
+      if (wrapper && wrapper.classList.contains('w-checkbox-input')) {
+        if (checkedState[cb.id]) {
+          wrapper.classList.add('w--redirected-checked');
+        } else {
+          wrapper.classList.remove('w--redirected-checked');
+        }
+      }
     });
     
     if (checkedState.All) {
